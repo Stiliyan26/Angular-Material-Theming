99049289045928684196fd35bded8e007ba3ab0f
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2023-03-21 13:36:00 +0100

    chor(Project) Create an Angular 15 project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true
 })
 export class AppComponent {
-  title = 'angular-material-theming-masterclass';
+
 }
